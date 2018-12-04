cbc682dd97f2655639097af54bef89886290f1b6
David Katz <15Dkatz@shcp.edu>
2018-12-04 07:40:15 -0800

    Use single quotes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { random_joke, random_ten } = require('./handler');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
