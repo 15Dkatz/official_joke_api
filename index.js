6361aa1e15bc6384a00e4b23fd7a14d03b6bb10d
David Katz <15Dkatz@shcp.edu>
2019-01-06 11:03:59 -0800

    Implement rate limiting for Joke API

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const { randomJoke, randomTen } = require('./handler');
+const LimitingMiddleware = require('./limiting-middleware');
 
 const app = express();
+
+const limitingMiddleware = new LimitingMiddleware();
+
+app.use(limitingMiddleware.limitByIp());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Try /random_joke or /random_ten');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
 });
 
 app.get('/random_joke', (req, res) => {
@@ -14,6 +27,14 @@ app.get('/random_joke', (req, res) => {
 
 app.get('/random_ten', (req, res) => {
   res.json(randomTen());
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error', message: err.message
+  });
 });
 
 const PORT = process.env.PORT || 3000;
