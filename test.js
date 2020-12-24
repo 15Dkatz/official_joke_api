b0b92c39d5058932b09ae4f0237f2fccb5d3da81
Dru Serkes <DruSerkes@gmail.com>
2020-12-24 01:51:48 -0500

    add testcase for invalid input

--- test.js
+++ test.js
@@ -5,11 +5,17 @@ console.log('randomTen', randomTen());
 
 it('should render the joke with an id of 1', () => {
     expect(jokeById(1)).toEqual(
-        [{
+        {
             "id": 1,
             "type": "general",
             "setup": "What did the fish say when it hit the wall?",
             "punchline": "Dam."
-        }]
+        }
     );
+});
+
+it('should return undefined with an invalid id', () => {
+    expect(jokeById('one')).toEqual(undefined);
+    expect(jokeById('1')).toEqual(undefined);
+    expect(jokeById()).toEqual(undefined);
 })
