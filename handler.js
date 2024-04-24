87b41784011870177df781cda4364407311f6197
Samil Abud <samilabud@gmail.com>
2024-04-23 23:40:11 -0400

    Added pagination method in express and the handler function where sort is handle too.

--- handler.js
+++ handler.js
@@ -1,11 +1,48 @@
-const jokes = require('./jokes/index.json');
+const jokes = require("./jokes/index.json");
 
 let lastJokeId = 0;
-jokes.forEach(jk => jk.id = ++lastJokeId);
+jokes.forEach((jk) => (jk.id = ++lastJokeId));
 
 const randomJoke = () => {
   return jokes[Math.floor(Math.random() * jokes.length)];
-}
+};
+
+/**
+ * Get jokes in specific order for pagination purpose
+ * @param {Number} pageSize - Number of items per page
+ * @param {Number} pageNumber - Page number
+ * @param {String} sortKey - The key to sort the jokes "type" | "setup" | "punchline"
+ * @param {String} sortOrder - How to sort the jokes "asc" | "desc"
+ * @returns an array of jokes
+ */
+
+const paginateJokes = (
+  pageSize,
+  pageNumber,
+  sortKey = null,
+  sortOrder = "asc"
+) => {
+  // Zero-based index for the current page slice
+  --pageNumber;
+
+  // Apply sorting if sortKey is provided
+  if (sortKey) {
+    jokes.sort((a, b) => {
+      const valueA =
+        typeof a[sortKey] === "string" ? a[sortKey].toUpperCase() : a[sortKey];
+      const valueB =
+        typeof b[sortKey] === "string" ? b[sortKey].toUpperCase() : b[sortKey];
+
+      if (sortOrder.toLowerCase() === "asc") {
+        return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
+      } else {
+        return valueA > valueB ? -1 : valueA < valueB ? 1 : 0;
+      }
+    });
+  }
+
+  return jokes.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
+};
 
 /**
  * Get N random jokes from a jokeArray
@@ -21,7 +58,7 @@ const randomN = (jokeArray, n) => {
     }
   }
 
-  return Array.from(randomIndicesSet).map(randomIndex => {
+  return Array.from(randomIndicesSet).map((randomIndex) => {
     return jokeArray[randomIndex];
   });
 };
@@ -31,13 +68,25 @@ const randomTen = () => randomN(jokes, 10);
 const randomSelect = (number) => randomN(jokes, number);
 
 const jokeByType = (type, n) => {
-  return randomN(jokes.filter(joke => joke.type === type), n);
+  return randomN(
+    jokes.filter((joke) => joke.type === type),
+    n
+  );
 };
 
-/** 
+/**
  * @param {Number} id - joke id
  * @returns a single joke object or undefined
  */
-const jokeById = (id) => (jokes.filter(jk => jk.id === id)[0]);
+const jokeById = (id) => jokes.filter((jk) => jk.id === id)[0];
 
-module.exports = { jokes, randomJoke, randomN, randomTen, randomSelect, jokeById, jokeByType };
+module.exports = {
+  jokes,
+  randomJoke,
+  randomN,
+  randomTen,
+  randomSelect,
+  jokeById,
+  jokeByType,
+  paginateJokes,
+};
--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const express = require('express');
-const LimitingMiddleware = require('limiting-middleware');
-const { randomJoke, randomTen, randomSelect, jokeByType, jokeById } = require('./handler');
+const express = require("express");
+const LimitingMiddleware = require("limiting-middleware");
+const {
+  randomJoke,
+  randomTen,
+  randomSelect,
+  jokeByType,
+  jokeById,
+  paginateJokes,
+} = require("./handler");
 
 const app = express();
 
 app.use(new LimitingMiddleware().limitByIp());
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Try /random_joke, /random_ten, /jokes/random, or /jokes/ten');
+app.get("/", (req, res) => {
+  res.send("Try /random_joke, /random_ten, /jokes/random, or /jokes/ten");
 });
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
-app.get('/random_joke', (req, res) => {
+app.get("/random_joke", (req, res) => {
   res.json(randomJoke());
 });
 
-app.get('/random_ten', (req, res) => {
+app.get("/random_ten", (req, res) => {
   res.json(randomTen());
 });
 
-app.get('/jokes/random', (req, res) => {
+app.get("/jokes/random", (req, res) => {
   res.json(randomJoke());
 });
 
@@ -50,26 +58,48 @@ app.get("/jokes/random(/*)?", (req, res) => {
   }
 });
 
-app.get('/jokes/ten', (req, res) => {
+app.get("/jokes/ten", (req, res) => {
   res.json(randomTen());
 });
 
-app.get('/jokes/:type/random', (req, res) => {
+app.get("/jokes/:type/random", (req, res) => {
   res.json(jokeByType(req.params.type, 1));
 });
 
-app.get('/jokes/:type/ten', (req, res) => {
+app.get("/jokes/:type/ten", (req, res) => {
   res.json(jokeByType(req.params.type, 10));
 });
 
-app.get('/jokes/:id', (req, res, next) => {
+app.get("/jokes/:id", (req, res, next) => {
   try {
     const { id } = req.params;
     const joke = jokeById(+id);
-    if (!joke) return next({ statusCode: 404, message: 'joke not found' });
+    if (!joke) return next({ statusCode: 404, message: "joke not found" });
     return res.json(joke);
   } catch (e) {
     return next(e);
+  }
+});
+
+app.get("/paginateJokes/", (req, res) => {
+  const { pageSize, pageNumber, sortKey = null, sortOrder } = req.body;
+  const validSortKeys = ["type", "setup", "punchline"];
+  const validSortOrders = ["asc", "desc"];
+  console.log({ pageSize, pageNumber, sortKey, sortOrder });
+  try {
+    if (isNaN(pageSize)) {
+      res.send("The pageSize is not a number.");
+    } else if (isNaN(pageNumber)) {
+      res.send("The pageNumber is not a number.");
+    } else if (sortKey !== null && !validSortKeys.includes(sortKey)) {
+      res.send(`The sortKey valids are ${validSortKeys.join(",")}.`);
+    } else if ((sortKey !== null) & !validSortOrders.includes(sortOrder)) {
+      res.send(`The sortOrder valids are ${validSortOrders.join(",")}.`);
+    } else {
+      res.json(paginateJokes(pageSize, pageNumber, sortKey, sortOrder));
+    }
+  } catch (err) {
+    res.send(`Error during pagination. ${err}`);
   }
 });
 
@@ -77,7 +107,8 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
   res.status(statusCode).json({
-    type: 'error', message: err.message
+    type: "error",
+    message: err.message,
   });
 });
 
